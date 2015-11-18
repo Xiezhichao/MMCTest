61861aff472ebaf07488f6a7319d02a4180186b2
xiezhichao <xiezhichao@mmclick.com>
2015-11-18 12:26:25 +0800

    第一次测试提交

---
+++ test1/MyTest.h
@@ -0,0 +1,13 @@
+//
+//  MyTest.h
+//  FrameworkDemo
+//
+//  Created by mmclick on 15/7/28.
+//  Copyright (c) 2015年 MMC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyTest : NSObject
++ (BOOL)Log:(NSString *)str;
+@end
---
+++ test1/MyTest.m
@@ -0,0 +1,18 @@
+//
+//  MyTest.m
+//  FrameworkDemo
+//
+//  Created by mmclick on 15/7/28.
+//  Copyright (c) 2015年 MMC. All rights reserved.
+//
+
+#import "MyTest.h"
+
+@implementation MyTest
+
++ (BOOL)Log:(NSString *)str {
+    NSLog(@"输出语句：%@",str);
+    return YES;
+}
+
+@end
